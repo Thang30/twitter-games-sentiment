b6f14c230614872f99a85d880b1a99ff44625e70
Thang30 <fsnthang30@gmail.com>
2017-03-20 10:55:19 +0200

    add analysis notebook

--- helpers/analysis.py
+++ helpers/analysis.py
@@ -2,9 +2,9 @@ from helpers import settings
 
 
 def tweet_lengths(text):
-    if len(text) < 50:
+    if len(text) < 100:
         return "short"
-    elif 50 <= len(text) <= 100:
+    elif 100 <= len(text) <= 135:
         return "medium"
     else:
         return "long"
@@ -13,7 +13,8 @@ def tweet_lengths(text):
 def get_games(row):
     games = []
     text = row["text"].lower()
-    for game in settings.GAME_LIST:
+    games_list = [x.lower() for x in settings.GAME_LIST]
+    for game in games_list:
         if game in text:
             games.append(game)
     return ",".join(games)
